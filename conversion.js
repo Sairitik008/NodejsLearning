ad2dfc4a61c86f3185aae3421ca45eb9a25a194b
Sairitik008 <sairitiknaidu7@gmail.com>
2025-12-01 14:47:48 +0530

    stringand number precendence

---
+++ conversion.js
@@ -0,0 +1,36 @@
+let score = "33"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+
+let valueInNumber = Number(score)
+console.log(typeof (valueInNumber))
+
+//type of number will NAN if you check for alphanumeric value
+
+
+//Another data conversion into number
+//"33"=> convert in number easily -33
+//"33abc"=>NaN
+//true=>1;false =>0
+
+
+
+//number into boolean
+//true conversion of one into true for false it will be 0
+// empty string = false
+//string with value=true
+let isLoggined = 1
+
+let booleanIsLoggedIn = Boolean(isLoggined)
+console.log(booleanIsLoggedIn)
+
+
+//Number to String
+let sum = 45
+let StringNumber = String(sum)
+console.log(StringNumber)
+console.log(typeof StringNumber)
+
+
---
+++ video7.js
@@ -0,0 +1,9 @@
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+console.log(typeof negvalue)
+
+console.log("1" + 2);//12
+console.log(1 + "2")//12
+console.log(3 + 2 + "4")//54
+console.log(((5 * 4 * 2) / 3)%2)
